258f285aad01b9606522a1931ff7e0972701834c
unknown <emanuel.bumbuta@gmail.com>
2024-12-31 02:38:44 +0200

    Add front end

--- src/main/java/com/bumbi/to_do_list/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bumbi.to_do_list.repository;
-
-import com.bumbi.to_do_list.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item,Integer> {
-}
