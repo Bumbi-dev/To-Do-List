1d2344fd3d96a9a032476329a94a0067e7a05e33
unknown <emanuel.bumbuta@gmail.com>
2025-01-19 16:53:09 +0200

    Create all the tasks and handle color

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -2,21 +2,29 @@ import './style.css'
 import { Task } from './task.ts'
 
 const list = document.getElementById("task-list") as HTMLElement
-const child = document.getElementById("task-example")!.cloneNode(true) as HTMLElement
 
-const req = await fetch("http://localhost:8080/closestTask")!
-const resp = await req.json()
+await initTasks()
 
-createTaskFromObject(resp)
+async function initTasks() {
+    const req = await fetch("http://localhost:8080/tasks")!
+    const resp = await req.json()
 
-function createTaskFromObject(taskObject: any) {
-    const task = new Task(taskObject)
-    child.classList.remove('hidden')
+    for (let i = 0; i < resp.length; i++) {
+      createTaskFromObject(resp[i]);
+    }
+}
+
+function createTaskFromObject(taskObject: any): boolean {
+    const child = document.getElementById("task-example")!.cloneNode(true) as HTMLElement;
+    const task = new Task(taskObject);
     
-    child.querySelector(".desc")!.textContent = task.desc
-    child.querySelector(".creation-date")!.textContent = task.getCreationDate()
-    child.querySelector(".due-date")!.textContent = task.getDueDate()
-    child.setAttribute("style", task.color);
+    child.classList.remove('hidden');
+    
+    child.querySelector(".desc")!.textContent = task.desc;
+    child.querySelector(".creation-date")!.textContent = task.getCreationDate();
+    child.querySelector(".due-date")!.textContent = task.getDueDate();
+    child.setAttribute("style", `color: ${task.color};`);
 
-    list.appendChild(child)
+    list.appendChild(child);
+    return true;
 }
